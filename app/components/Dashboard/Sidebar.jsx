105a1cd7d3af9e5bafc60cb0ee995cb64b13b4e0
suyashpurwar1 <suyashpurwar1@gmail.com>
2024-11-18 13:11:32 +0530

    improved sidebar css

--- app/components/Dashboard/Sidebar.jsx
+++ app/components/Dashboard/Sidebar.jsx
@@ -5,20 +5,20 @@ const Sidebar = () => (
   <aside className="w-55 bg-gray-50 p-4 h-screen shadow border-slate-300">
     <nav className="space-y-6">
       <br />
-      <a className="flex items-center text-black hover:text-blue-500 space-x-2 space-y-2.5">
-        <FaTachometerAlt />
+      <a className="flex items-center text-black hover:text-blue-500 space-x-2 pl-7">
+        <FaTachometerAlt className="mr-2" />
         <span>
           <strong>Dashboard</strong>
         </span>
       </a>
-      <a className="flex items-center text-black hover:text-blue-500 space-x-2 space-y-2.5">
-        <FaMedal />
+      <a className="flex items-center text-black hover:text-blue-500 space-x-2 pl-7">
+        <FaMedal className="mr-2" />
         <span>
           <strong>Skill Test</strong>
         </span>
       </a>
-      <a className="flex items-center text-black hover:text-blue-500 space-x-2 space-y-2.5">
-        <FaClipboard />
+      <a className="flex items-center text-black hover:text-blue-500 space-x-2 pl-7">
+        <FaClipboard className="mr-2" />
         <span>
           <strong>Internship</strong>
         </span>
